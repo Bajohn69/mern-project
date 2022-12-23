23fb74508abb94b8d615b633759958fb110d16d3
Bajohn Huang <jimmybaga@hotmail.com>
2022-12-23 17:21:09 +0800

    heroku version

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ const passport = require("passport");
 require("./config/passport")(passport);
 // 這裡面是一個 function 代入 passport 參數直接執行
 const cors = require("cors");
+// --------- heroku 新增-----------------
+const path = require("path");
+const port = process.env.PORT || 8080;
+// process.env.PORT 是 heroku 自動動態設定的
+// --------- heroku 新增-----------------
 
 mongoose
-  .connect("mongodb://localhost:27017/mernDB")
+  .connect(process.env.MONGODB_CONNECTION)
   .then(() => {
     console.log("Connecting to mongoDB");
   })
@@ -25,6 +30,9 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+// --------- heroku 新增-----------------
+app.use(express.static(path.join(__dirname, "client", "build")));
+// --------- heroku 新增-----------------
 
 app.use("/api/user", authRoute);
 app.use(
@@ -35,7 +43,20 @@ app.use(
 // 要被 jwt 保護，只有登入系統的人，才能新增或註冊課程(驗證 jwt)
 // 如果 request header 內部沒有 jwt 則 request 會被視為是 unauthorized
 
+// --------- heroku 新增-----------------
+// 只要網頁放到雲端的伺服器時，若有接收到除了上方兩個之外的 request 就會到下面把 index.html 送給使用者
+// heroku 自動設定的環境變數 NODE_ENV
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+// --------- heroku 新增-----------------
+
 // react default is port 3000 要錯開
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("Server is running on port 8080.");
 });
