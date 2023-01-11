bbcf5bd4bb1728d1a5afd61b2ce5017afcde596e
Bajohn Huang <jimmybaga@hotmail.com>
2023-01-11 11:45:45 +0800

    modified home component

--- client/src/components/home-component.js
+++ client/src/components/home-component.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AuthService from "../services/auth.service";
 
 const HomeComponent = () => {
   return (
@@ -12,9 +14,6 @@ const HomeComponent = () => {
               作為後端服務器。這種項目稱為 MERN
               項目，它是創建現代網站的最流行的方式之一。
             </p>
-            <button className="btn btn-primary btn-lg" type="button">
-              看看它怎麼運作。
-            </button>
           </div>
         </div>
 
@@ -25,9 +24,11 @@ const HomeComponent = () => {
               <p>
                 學生可以註冊他們喜歡的課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
               </p>
-              <button className="btn btn-outline-light" type="button">
-                登錄會員、或者註冊一個帳號
-              </button>
+              <Link className="nav-link" to="/register">
+                <button className="btn btn-outline-light" type="button">
+                  登錄會員、或者註冊一個帳號
+                </button>
+              </Link>
             </div>
           </div>
           <div className="col-md-6">
@@ -36,15 +37,17 @@ const HomeComponent = () => {
               <p>
                 您可以通過註冊成為一名講師，並開始製作在線課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
               </p>
-              <button className="btn btn-outline-secondary" type="button">
-                今天開始開設課程
-              </button>
+              <Link className="nav-link" to="/register">
+                <button className="btn btn-outline-secondary" type="button">
+                  今天開始開設課程
+                </button>
+              </Link>
             </div>
           </div>
         </div>
 
         <footer className="pt-3 mt-4 text-muted border-top">
-          &copy; 2023 Wilson Ren
+          &copy; 2023 Bajohn Huang
         </footer>
       </div>
     </main>
